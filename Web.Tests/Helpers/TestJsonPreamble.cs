22631cb2b2943e5a997af5319a635b7d3b971c4d
t3hmun <git@t3hmun.com>
2019-09-08 10:19:42 +0100

    Fixed failing test created new failing test.

--- Web.Tests/Helpers/TestJsonPreamble.cs
+++ Web.Tests/Helpers/TestJsonPreamble.cs
@@ -7,12 +7,15 @@
     public class TestPreambleProcessor
     {
         private const string SimpleDescription = "This is a dsescription.";
-        private const string SimpleDescriptionJson = "{\"Description\":\"" + SimpleDescription + "\"}";
+        private const string SimpleDescriptionJson = "{\"description\":\"" + SimpleDescription + "\"}";
         private const string SimpleBody = "#h1\n\nSomeText\n";
+        private const string MultilineDescriptionJson = "{\n\"description\": " + SimpleDescription + "\"\n}";
         private const string SimpleDescriptionSut = SimpleDescriptionJson + "\n" + SimpleBody;
+        private const string MultilineJsonSimpledDescriptionSut = MultilineDescriptionJson + "\n" + "SimpleBody";
 
         [TestMethod]
         [DataRow(SimpleDescriptionSut, SimpleDescription, SimpleBody)]
+        [DataRow(MultilineJsonSimpledDescriptionSut, SimpleDescription, SimpleBody)]
         public void TestJsonPreamble(string sut, string expectedDescription, string expectedBody)
         {
             var post = new Post();
